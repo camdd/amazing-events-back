49d717fa9a9471a6e6f6c8e4ffb3713c20595d3f
Cam <camila.dambrosio95@gmail.com>
2023-11-23 12:58:39 -0300

    método post finalizado en swagger

--- src/app.js
+++ src/app.js
@@ -2,12 +2,16 @@ const express = require('express');
 require("dotenv").config()
 require('./db.js')
 
+const swaggerUi = require("swagger-ui-express")
+const swaggerDocumentConfig = require("../swagger-openapi.json")
+
 const indexRouter = require('./router/indexRouter.js')
 
 const app = express();
 app.use( express.json() )
 app.use( "/api", indexRouter )
 
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocumentConfig))
 
 app.listen(3000, ()=> {
     console.log('listening on port 3000')
